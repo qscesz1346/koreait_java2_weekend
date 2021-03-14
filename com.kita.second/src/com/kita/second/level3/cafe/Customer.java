10ba3f9a5f14c31ad87bbb131b0cd8d0ed7ecd79
qscesz1346 <qscesz1346@naver.com>
2021-03-15 01:43:09 +0900

    21.03.14 카페 숙제

--- com.kita.second/src/com/kita/second/level3/cafe/Customer.java
+++ com.kita.second/src/com/kita/second/level3/cafe/Customer.java
@@ -5,10 +5,20 @@ import java.util.Scanner;
 public class Customer {
 	public MenuItem order(Menu m) {
 		m.showMenus();
-		Scanner scan = new Scanner(system.in);
+		
+		Scanner scan = new Scanner(System.in);
+		MenuItem mi = null;
+		
+		while(mi == null) {
+			m.choose(scan.nextInt());
+			
+			
+			break;
+		}
+		scan.close();
 		return mi;
 	}
-	
+
 	void drinkCoffee(Coffee c) {
 		String name = c.getName();
 		System.out.println(name + "(을)를 마신다.\n");
--- com.kita.second/src/com/kita/second/level3/cafe/Menu.java
+++ com.kita.second/src/com/kita/second/level3/cafe/Menu.java
@@ -1,21 +1,24 @@
 package com.kita.second.level3.cafe;
 
 public class Menu {
-	private String name;
-	private int price;
+	private MenuItem[] arr = new MenuItem[4];
 	
-	public Menu() {}
-	
-	public Menu(MenuItem mi) {
-		name = mi.getName();
-		price = mi.getPrice();
+	public Menu() {
+		arr[0] = new MenuItem("아메리카노", 2000);
+		arr[1] = new MenuItem("카페라떼", 2300);
+		arr[2] = new MenuItem("카페모카", 2500);
+		arr[3] = new MenuItem("카라멜마끼아또", 2600);
 	}
 	
-	public String getName() {
-		return name;
+	public void showMenus () {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.printf("%s\n", arr[i]);
+		}
 	}
 	
-	public int getPrice() {
-		return price;
+	public MenuItem choose(int idx) {
+		return arr[idx];
 	}
+	
+	
 }
